c8ec7ea1477db29f497f0413eba5ade790225de1
Anshuman Srivastava <srivastavaanshuman33@gmail.com>
2020-07-29 16:46:21 +0530

    changed_method

--- constants.py
+++ constants.py
@@ -1,2 +1,2 @@
-filename="Sample8.pdf"
+filename="Sample3.pdf"
 tesseract_path= r'C:\Users\sriva\AppData\Local\Tesseract-OCR\tesseract.exe'
--- tables.py
+++ tables.py
@@ -14,18 +14,20 @@ path_to_read= Path.joinpath(path_to_write,"Intermediates")
 def get_data():
     print("----Getting tables-----")
     vert= cv2.imread(str(Path.joinpath(path_to_read,'verticle_lines.jpg')))
+    horzt= cv2.imread(str(Path.joinpath(path_to_read,'horizontal_lines.jpg')))
     actual=cv2.imread(str(Path.joinpath(path_to_read,'Image_bin.jpg')))
     actual =cv2.bitwise_not(actual,mask=None)
     ar= np.array(vert);
+    ahr= np.array(horzt)
     height,width,channel= ar.shape
-
+    
     color=ar[0][0]
     xi=None 
     xj=None
     flag=0
     for i in range(height):
         c=0
-        for j in range(140,width,5):
+        for j in range(20,width,5):
             comp= (sum(ar[i][j])>20)
             if(comp):
                 c+=1
@@ -43,95 +45,82 @@ def get_data():
     # xi=427
     # xj=2259
     dist=[]
-    for j in range(0,width):
+    for j in range(0,width,6):
         mid=(xi+xj)//2
-        color=[0,0,0]
+        # print(ar[mid][j])
         comp= sum(ar[mid][j])>20
         if(comp):
             dist.append(j)
 
     # print(dist)
-
+     
     # for k in dist:
         # actual=cv2.line(actual,(k,0),(k,height),(0,255,255),3)
-    vert_lines=[]
+    vert_lines=dist
     horizontal_lines=[]
-    cnt=0
-    for i in range(1,len(dist)):
-        if (dist[i]-dist[i-1])>8:
-            if cnt==0: 
-                vert_lines.append(dist[i-1])
-                cnt+=1
-            vert_lines.append(dist[i])
-    for i in range(xi,xj):
+    # cnt=0
+    # for i in range(1,len(dist)):
+    #     if (dist[i]-dist[i-1])>8:
+    #         if cnt==0: 
+    #             vert_lines.append(dist[i-1])
+    #             cnt+=1
+    #         vert_lines.append(dist[i])
+    for i in range(xi,xj+1,5):
         mid= (vert_lines[0]+vert_lines[-1])//2
-        if sum(ar[i][mid])>20:
+        if sum(ahr[i][mid])>20:
             horizontal_lines.append(i)
-
-    hts=[]
-    for i in range(1,len(horizontal_lines)):
-        hts.append(horizontal_lines[i]-horizontal_lines[i-1])
+    
+    #==============UNCOMMENT TO SEE THE VERTICAL AND HORIZONTAL LINES=============
+    # mid= (vert_lines[0]+vert_lines[-1])//2
+    # imk= cv2.line(actual,(mid,0),(mid,height),(0,255,0),10)
+    # imk=actual
+    # for ho in horizontal_lines:
+    #     imk= cv2.line(imk,(0,ho),(width,ho),(255,0,0),10)
+    # for vo in vert_lines:
+    #     imk= cv2.line(imk,(vo,0),(vo,height),(255,0,0),10)
+    #  cv2.imwrite(str(Path.joinpath(path_to_read,"tables_lens.jpg")),imk)
+    # read= cv2.imread('mix.jpg')
+    # read= cv2.resize(read,(1080,720))
+    # cv2.imshow('ime',read)
+    # cv2.waitKey(0)
 
     w=width
     h=100
-    if len(hts)>1:
-        h=int(hts[1])
-    print(h)
     # img= actual[k:k+h,:]
     # actual=cv2.resize(actual,(800,960))
     # cv2.imshow("sdfe",actual)
     # cv2.waitKey(0)
     n=len(vert_lines)
-    # print(vert_lines)
+    print(vert_lines)
+    print(horizontal_lines)
     total_length=vert_lines[n-1]-vert_lines[0]
     tables=[]
+    for i in range(len(horizontal_lines)-1):
+        text=[]
+        for j in range(len(vert_lines)-1):
+            x=horizontal_lines[i]
+            y=vert_lines[j]
+            w=vert_lines[j+1]-vert_lines[j]
+            h=horizontal_lines[i+1]-horizontal_lines[i]
+            img=actual[x:x+h,y:y+w]
+            r= Path.joinpath(path_to_write,"Rows")
+            im_no="row"+str(i+j)+'.jpg'
+            cv2.imwrite(str(Path.joinpath(r,im_no)),img)
+            custom_config = r"--oem 3 --psm 6"
+            # pytesseract.run_tesseract("rows/row"+str(m)+".jpg","output_hocr1",extension='jpg', lang=None,config=custom_config)
+            txt= pytesseract.image_to_string(str(Path.joinpath(r,im_no)), lang=None,config=custom_config)
+            text.append(str(txt))
+        tables.append(text)
     
-    m=0
-    k=xi
-    heading=True
-    while k+h<xj:
-        # print('***'*39)
-        img= actual[k:k+h,:]
-        r= Path.joinpath(path_to_write,"Rows")
-        im_no="row"+str(m)+'.jpg'
-        cv2.imwrite(str(Path.joinpath(r,im_no)),img)
-        custom_config = r"--oem 3 --psm 3"
-        # pytesseract.run_tesseract("rows/row"+str(m)+".jpg","output_hocr1",extension='jpg', lang=None,config=custom_config)
-        text= pytesseract.image_to_string(str(Path.joinpath(r,im_no)), lang=None,config=custom_config)
-        m+=1
-        k+=h
-        if(len(str(text))<10): continue
-        
-        text=(str(text))
-        if heading:
-            heading=False
-            text=text.replace(" ","|")
-            text=text.split("|")
-            colnames=[]
-            for name in text:
-                if len(name)>1:
-                    colnames.append(name) 
-            tables.append(colnames)
-            continue
-        text=text.replace("|"," ")
-        text=text.split()
-        # print(text)
-        
-        splitted=[text[0]]
-        txt_len=len(text)
-        x=1
-        for i in range(2,3):
-            ratio=(vert_lines[i]-vert_lines[i-1])/total_length
-            t= round(ratio*txt_len)
-            end=min(x+t+1,len(text)-1)
-            splitted.append(" ".join(text[x:end]))
-            x=end
-        for i in range(x,len(text)):
-            if len(text[i])>1:
-                splitted.append(text[i])
-        if len(splitted)>3:
-            tables.append((splitted))
+    # m=0
+    # k=xi
+    # heading=True
     
+    # # print('***'*39)
+    # img= actual[k:k+h,:]
+    
+    # m+=1
+    # k+=h
     wid= max([len(i) for i in tables])
     tables= pad(tables,wid,"NULL")
     return np.array(tables)
